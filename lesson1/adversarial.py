1bb57a21fe94edc4a777a4d3137d4f6b1c5b8d7f
oliverzhang42 <oliver.zhang.42@gmail.com>
2021-08-19 15:12:00 +0100

    First Lesson

--- lesson1/adversarial.py
+++ lesson1/adversarial.py
@@ -1,8 +1,13 @@
 # Everything Necessary for the Adversarial Examples Class
+import json
 import numpy as np
 import torch
 import torchvision
 
+def get_imagenet_labels():
+    with open('safety/lesson1/imagenet_labels.json', 'r') as f:
+        index_to_label = json.load(f)
+    return index_to_label
 
 def test_FGSM(FGSM_Implementation):
     pass
