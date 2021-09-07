c72a95ca727185fafcde5bbc806ad31253964402
oliverzhang42 <oliver.zhang.42@gmail.com>
2021-09-07 09:52:45 -0400

    Changes to first lesson

--- lesson1/adversarial.py
+++ lesson1/adversarial.py
@@ -3,9 +3,31 @@ import json
 import numpy as np
 import torch
 import torchvision
+from safety.utils import utils
+from torch import nn
+from torchvision import models
 
 
-def get_imagenet_labels():
-    with open('safety/lesson1/imagenet_labels.json', 'r') as f:
-        index_to_label = json.load(f)
-    return index_to_label
+def test_untargeted_FGSM(untargeted_FGSM):
+  # Load the models
+  model = models.resnet18(pretrained=True, progress=False).eval()
+  print('')
+  
+  # Load the preprocessed image
+  image, true_index = utils.load_example_image(preprocess=True)
+  true_label = utils.get_imagenet_label(true_index)
+  print(f'The correct label is "{true_label}"')
+
+  # Generate predictions
+  _, index, confidence = utils.make_single_prediction(model, image)
+  label = utils.get_imagenet_label(index)
+  print(f'The original prediction was {label} with logit value {confidence}.')
+
+  # Generate Adversarial Example
+  true_index = torch.Tensor([true_index]).type(torch.long)
+  adv_image = untargeted_FGSM(image, true_index, model, 0.01)
+
+  # Display Results
+  _, adv_index, adv_confidence = utils.make_single_prediction(model, adv_image)
+  adv_label = utils.get_imagenet_label(adv_index)
+  print(f'The adversarial prediction was {adv_label} with logit value {adv_confidence}.')
