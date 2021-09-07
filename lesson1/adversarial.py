868034061a5a2a6230bc19d00b4960d96965c3d9
oliverzhang42 <oliver.zhang.42@gmail.com>
2021-09-07 13:05:03 -0400

    First Lesson

--- lesson1/adversarial.py
+++ lesson1/adversarial.py
@@ -2,11 +2,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn.functional as F
 from safety.utils import utils
 from torchvision import models
 
 
-def test_untargeted_FGSM(untargeted_FGSM, eps=0.01):
+def test_untargeted_attack(untargeted_adv_attack, eps=0.01):
     # Load the models
     model = models.resnet18(pretrained=True, progress=False).eval()
     print('')
@@ -21,7 +22,7 @@ def test_untargeted_FGSM(untargeted_FGSM, eps=0.01):
 
     # Generate Adversarial Example
     true_index = torch.Tensor([true_index]).type(torch.long)
-    adv_image = untargeted_FGSM(image.unsqueeze(0), true_index, model, eps=eps).squeeze(0)
+    adv_image = untargeted_adv_attack(image.unsqueeze(0), true_index, model, eps=eps).squeeze(0)
 
     # Display Results
     _, adv_index, adv_confidence = utils.make_single_prediction(model, adv_image.squeeze(0))
@@ -43,7 +44,7 @@ def test_untargeted_FGSM(untargeted_FGSM, eps=0.01):
     plt.show()
 
 
-def test_targeted_FGSM(targeted_FGSM, target_idx=10, eps=0.01):
+def test_targeted_attack(targeted_adv_attack, target_idx=10, eps=0.01):
     # Load the models
     model = models.resnet18(pretrained=True, progress=False).eval()
     print('')
@@ -62,7 +63,8 @@ def test_targeted_FGSM(targeted_FGSM, target_idx=10, eps=0.01):
     print(f'The target index corresponds to a label of {target_label}!')
 
     # Generate Adversarial Example
-    adv_image = targeted_FGSM(image.unsqueeze(0), [target_idx], model, eps=eps).squeeze(0)
+    target_idx = torch.Tensor([target_idx]).type(torch.long)
+    adv_image = targeted_adv_attack(image.unsqueeze(0), target_idx, model, eps=eps).squeeze(0)
 
     # Display Results
     _, adv_index, adv_confidence = utils.make_single_prediction(model, adv_image.squeeze(0))
@@ -84,89 +86,37 @@ def test_targeted_FGSM(targeted_FGSM, target_idx=10, eps=0.01):
     plt.show()
 
 
-def untargeted_PGD():
-    pass
+def untargeted_PGD(x_batch, true_labels, network, num_steps=10, step_size=0.003, eps=0.01):
+    """Generates a batch of untargeted PGD adversarial examples
+
+    Args:
+        x_batch (torch.Tensor): the batch of preprocessed input examples.
+        true_labels (torch.Tensor): the batch of true labels of the example.
+        network (nn.Module): the network to attack.
+        num_steps (int): the number of steps to run PGD.
+        step_size (float): the size of each PGD step.
+        eps (float): the bound on the perturbations.
+    """
+    adv_x = x_batch.detach()
+    adv_x += torch.zeros_like(adv_x).uniform_(eps, eps)
+
+    for i in range(num_steps):
+        adv_x.requires_grad_()
+        
+        # Calculate gradients
+        with torch.enable_grad():
+            logits = network(adv_x) #TODO: Removed the 2*x - 1
+            loss = F.cross_entropy(logits, true_labels, reduction='sum')
+        grad = torch.autograd.grad(loss, adv_x, only_inputs=True)[0]
+        
+        # Perform one gradient step
+        adv_x = adv_x.detach() + step_size * torch.sign(grad.detach())
+        
+        # Project the image to the ball
+        adv_x = torch.maximum(adv_x, x_batch - eps)
+        adv_x = torch.minimum(adv_x, x_batch + eps)
+
+    return adv_x
 
 def targeted_PGD():
     pass
-
-'''
-class PGD(nn.Module):
-    def __init__(self, epsilon, num_steps, step_size, grad_sign=True):
-        super().__init__()
-        self.epsilon = epsilon
-        self.num_steps = num_steps
-        self.step_size = step_size
-        self.grad_sign = grad_sign
-
-    def forward(self, model, bx, by):
-        """
-        :param model: the classifier's forward method
-        :param bx: batch of images
-        :param by: true labels
-        :return: perturbed batch of images
-        """
-        adv_bx = bx.detach()
-        adv_bx += torch.zeros_like(adv_bx).uniform_(-self.epsilon, self.epsilon)
-
-        for i in range(self.num_steps):
-            adv_bx.requires_grad_()
-            with torch.enable_grad():
-                logits = model(adv_bx * 2 - 1)
-                loss = F.cross_entropy(logits, by, reduction='sum')
-            grad = torch.autograd.grad(loss, adv_bx, only_inputs=True)[0]
-
-            if self.grad_sign:
-                adv_bx = adv_bx.detach() + self.step_size * torch.sign(grad.detach())
-            else:
-                grad = normalize_l2(grad.detach())
-                adv_bx = adv_bx.detach() + self.step_size * grad
-
-            adv_bx = torch.min(torch.max(adv_bx, bx - self.epsilon), bx + self.epsilon).clamp(0, 1)
-
-        return adv_bx
-'''
-
-'''
-
-class PGDTarget(nn.Module):
-    def __init__(self, epsilon, num_steps, step_size, grad_sign=True):
-        super().__init__()
-        self.epsilon = epsilon
-        self.num_steps = num_steps
-        self.step_size = step_size
-        self.grad_sign = grad_sign
-
-    def forward(self, model, bx, by):
-        """
-        :param model: the classifier's forward method
-        :param bx: batch of images
-        :param by: true labels
-        :return: perturbed batch of images
-        """
-        adv_bx = bx.detach()
-        adv_bx += torch.zeros_like(adv_bx).uniform_(-self.epsilon, self.epsilon)
-
-        random_by = np.random.randint(0, 1000, by.size(0))
-        for i in range(len(random_by)):
-            while random_by[i] == by[i]:
-                random_by[i] = np.random.randint(1000)
-        random_by = torch.LongTensor(random_by).cuda()
-
-        for i in range(self.num_steps):
-            adv_bx.requires_grad_()
-            with torch.enable_grad():
-                logits = model(adv_bx * 2 - 1)
-                loss = -F.cross_entropy(logits, random_by, reduction='sum')
-            grad = torch.autograd.grad(loss, adv_bx, only_inputs=True)[0]
-
-            if self.grad_sign:
-                adv_bx = adv_bx.detach() + self.step_size * torch.sign(grad.detach())
-            else:
-                grad = normalize_l2(grad.detach())
-                adv_bx = adv_bx.detach() + self.step_size * grad
-
-            adv_bx = torch.min(torch.max(adv_bx, bx - self.epsilon), bx + self.epsilon).clamp(0, 1)
-
-        return adv_bx
-'''
