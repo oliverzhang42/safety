230726a8c5eaa061015b5dda1f1525f085c25a0f
oliverzhang42 <oliver.zhang.42@gmail.com>
2021-09-07 10:47:46 -0400

    First Lesson

--- lesson1/adversarial.py
+++ lesson1/adversarial.py
@@ -41,3 +41,44 @@ def test_untargeted_FGSM(untargeted_FGSM, eps=0.01):
     axes[1].set_title('Adversarial Image')
     axes[1].set(xlabel=f'Prediction: {adv_label}\nConfidence: {adv_confidence:.4f}')
     plt.show()
+
+
+def test_targeted_FGSM(targeted_FGSM, target_idx=10, eps=0.01):
+    # Load the models
+    model = models.resnet18(pretrained=True, progress=False).eval()
+    print('')
+    
+    # Load the preprocessed image
+    image, _ = utils.load_example_image(preprocess=True)
+
+    # Generate predictions
+    _, index, confidence = utils.make_single_prediction(model, image)
+    label = utils.get_imagenet_label(index)
+    label = label.split(',')[0]
+
+    # Get the target label
+    target_label = utils.get_imagenet_label(target_idx)
+    target_label = target_label.split(',')[0]
+    print(f'The target index corresponds to a label of {target_label}!')
+
+    # Generate Adversarial Example
+    adv_image = targeted_FGSM(image, target_idx, model, eps)
+
+    # Display Results
+    _, adv_index, adv_confidence = utils.make_single_prediction(model, adv_image)
+    adv_label = utils.get_imagenet_label(adv_index)
+    adv_label = adv_label.split(',')[0]
+
+    # Display Images
+    display_image = utils.IMAGENET_DENORMALIZE(image).detach().cpu().numpy()
+    display_image = np.moveaxis(display_image, 0, 2)
+    display_adv_image = utils.IMAGENET_DENORMALIZE(adv_image).detach().cpu().numpy()
+    display_adv_image = np.moveaxis(display_adv_image, 0, 2)
+    _, axes = plt.subplots(1,2)
+    axes[0].imshow(display_image)
+    axes[0].set_title('Original Image')
+    axes[0].set(xlabel=f'Prediction: {label}\nConfidence: {confidence:.4f}')
+    axes[1].imshow(display_adv_image)
+    axes[1].set_title('Adversarial Image')
+    axes[1].set(xlabel=f'Prediction: {adv_label}\nConfidence: {adv_confidence:.4f}')
+    plt.show()
