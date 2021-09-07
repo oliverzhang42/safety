ef4d3ed0b976f47507ce83cf909aa86c59f6cc9f
oliverzhang42 <oliver.zhang.42@gmail.com>
2021-09-07 13:57:28 -0400

    First Lesson

--- lesson1/adversarial.py
+++ lesson1/adversarial.py
@@ -118,5 +118,149 @@ def untargeted_PGD(x_batch, true_labels, network, num_steps=10, step_size=0.003,
 
     return adv_x
 
-def targeted_PGD():
-    pass
+
+def targeted_PGD(x_batch, target_labels, network, num_steps=10, step_size=0.01, eps=0.03):
+    """Generates a batch of untargeted PGD adversarial examples
+
+    Args:
+        x_batch (torch.Tensor): the batch of preprocessed input examples.
+        target_labels (torch.Tensor): the labels the model will predict after the attack.
+        network (nn.Module): the network to attack.
+        num_steps (int): the number of steps to run PGD.
+        step_size (float): the size of each PGD step.
+        eps (float): the bound on the perturbations.
+    """
+    adv_x = x_batch.detach()
+    adv_x += torch.zeros_like(adv_x).uniform_(eps, eps)
+
+    for i in range(num_steps):
+        adv_x.requires_grad_()
+        
+        # Calculate gradients
+        with torch.enable_grad():
+            logits = network(adv_x)
+            loss = F.cross_entropy(logits, target_labels, reduction='sum')
+        grad = torch.autograd.grad(loss, adv_x, only_inputs=True)[0]
+        
+        # Perform one gradient step
+        # Note that this time we use gradient descent instead of gradient ascent
+        adv_x = adv_x.detach() - step_size * torch.sign(grad.detach())
+        
+        # Project the image to the ball
+        adv_x = torch.maximum(adv_x, x_batch - eps)
+        adv_x = torch.minimum(adv_x, x_batch + eps)
+
+    return adv_x
+
+
+def attack_normal_model(
+    targeted_adv_attack, 
+    target_idx=10, 
+    eps=0.03, 
+    num_steps=10, 
+    step_size=0.01
+):
+    # Load the models
+    model = models.resnet18(pretrained=True, progress=False).eval()
+    print('')
+    
+    # Load the preprocessed image
+    image, _ = utils.load_example_image(preprocess=True)
+
+    # Generate predictions
+    _, index, confidence = utils.make_single_prediction(model, image)
+    label = utils.get_imagenet_label(index)
+    label = label.split(',')[0]
+
+    # Get the target label
+    target_label = utils.get_imagenet_label(target_idx)
+    target_label = target_label.split(',')[0]
+    print(f'The target index corresponds to a label of {target_label}!')
+
+    # Generate Adversarial Example
+    target_idx = torch.Tensor([target_idx]).type(torch.long)
+    adv_image = targeted_adv_attack(
+        image.unsqueeze(0), 
+        target_idx, 
+        model, 
+        eps=eps, 
+        num_steps=num_steps,
+        step_size=step_size
+    ).squeeze(0)
+
+    # Display Results
+    _, adv_index, adv_confidence = utils.make_single_prediction(model, adv_image.squeeze(0))
+    adv_label = utils.get_imagenet_label(adv_index)
+    adv_label = adv_label.split(',')[0]
+
+    # Display Images
+    display_image = utils.IMAGENET_DENORMALIZE(image).detach().cpu().numpy()
+    display_image = np.moveaxis(display_image, 0, 2)
+    display_adv_image = utils.IMAGENET_DENORMALIZE(adv_image).detach().cpu().numpy()
+    display_adv_image = np.moveaxis(display_adv_image, 0, 2)
+    _, axes = plt.subplots(1,2)
+    axes[0].imshow(display_image)
+    axes[0].set_title('Original Image')
+    axes[0].set(xlabel=f'Prediction: {label}\nConfidence: {confidence:.4f}')
+    axes[1].imshow(display_adv_image)
+    axes[1].set_title('Adversarial Image')
+    axes[1].set(xlabel=f'Prediction: {adv_label}\nConfidence: {adv_confidence:.4f}')
+    plt.show()
+
+
+def attack_adversarially_trained_model(
+    targeted_adv_attack, 
+    target_idx=10, 
+    eps=0.03, 
+    num_steps=10, 
+    step_size=0.01
+):
+    # Load the models
+    with open('safety/lesson1/resnet18_linf_eps8.0.ckpt', 'r') as f:
+        model = torch.load(f).eval()
+    print('')
+    
+    # Load the preprocessed image
+    image, _ = utils.load_example_image(preprocess=False)
+    image /= 255
+
+    # Generate predictions
+    _, index, confidence = utils.make_single_prediction(model, image)
+    label = utils.get_imagenet_label(index)
+    label = label.split(',')[0]
+
+    # Get the target label
+    target_label = utils.get_imagenet_label(target_idx)
+    target_label = target_label.split(',')[0]
+    print(f'The target index corresponds to a label of {target_label}!')
+
+    # Generate Adversarial Example
+    target_idx = torch.Tensor([target_idx]).type(torch.long)
+    adv_image = targeted_adv_attack(
+        image.unsqueeze(0), 
+        target_idx, 
+        model, 
+        eps=eps, 
+        num_steps=num_steps,
+        step_size=step_size
+    ).squeeze(0)
+
+    # Display Results
+    _, adv_index, adv_confidence = utils.make_single_prediction(model, adv_image.squeeze(0))
+    adv_label = utils.get_imagenet_label(adv_index)
+    adv_label = adv_label.split(',')[0]
+
+    # Display Images
+    display_image = image.detach().cpu().numpy()
+    display_image = np.moveaxis(display_image, 0, 2)
+    display_adv_image = adv_image.detach().cpu().numpy()
+    display_adv_image = np.moveaxis(display_adv_image, 0, 2)
+    _, axes = plt.subplots(1,2)
+    axes[0].imshow(display_image)
+    axes[0].set_title('Original Image')
+    axes[0].set(xlabel=f'Prediction: {label}\nConfidence: {confidence:.4f}')
+    axes[1].imshow(display_adv_image)
+    axes[1].set_title('Adversarial Image')
+    axes[1].set(xlabel=f'Prediction: {adv_label}\nConfidence: {adv_confidence:.4f}')
+    plt.show()
+
